d9d88f9045e2113cde5033da80abd18d70777a3d
Tayou <git@tayou.org>
2023-12-10 21:08:57 +0100

    added license file, license headers in code and SPDX license identifier for GPL-3.0

--- Scripts/Editor/AnimatorControllerExtensions.cs
+++ Scripts/Editor/AnimatorControllerExtensions.cs
@@ -3,7 +3,23 @@ using UnityEditor.Animations;
 using UnityEngine;
 using VRC.SDK3.Avatars.Components;
 using VRC.SDKBase;
-
+// SPDX-License-Identifier: GPL-3.0-only
+/*
+ *  Copyright (C) 2023 Tayou <git@tayou.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 namespace org.Tayou.AmityEdits {
     public static class AnimatorControllerExtensions {
         public static AnimatorControllerLayer NewLayer(this AnimatorController controller, 
