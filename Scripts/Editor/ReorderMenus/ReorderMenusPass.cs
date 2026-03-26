a1c9b0a175bcfce9f26f036d8181bd6f96ea6ef8
Tayou <git@tayou.org>
2026-03-26 17:35:58 +0100

    WIP add menu fixup steps

--- Scripts/Editor/ReorderMenus/ReorderMenusPass.cs
+++ Scripts/Editor/ReorderMenus/ReorderMenusPass.cs
@@ -31,6 +31,10 @@ namespace org.Tayou.AmityEdits {
     public class ReorderMenusPass {
         
         private readonly BuildContext _buildContext;
+        // private static Texture2D _moreIcon = AssetDatabase.LoadAssetAtPath<Texture2D>(
+        //     "Packages/nadena.dev.modular-avatar/Runtime/Icons/Icon_More_A.png"
+        // );
+        private static readonly string[] nextPageButtonNames = {"Next", "More", "Next Page"};
 
         public ReorderMenusPass(BuildContext context) {
             _buildContext = context;
@@ -40,9 +44,21 @@ namespace org.Tayou.AmityEdits {
             var avatarDescriptor = _buildContext.VRChatAvatarDescriptor();
             var rootMenu = avatarDescriptor.expressionsMenu;
             var components = avatarDescriptor.GetComponentsInChildren<ReorderMenus>(true);
-
-            var nextText = "Next";
+            var menuOptionsComponents = avatarDescriptor.GetComponentsInChildren<MenuOptions>(true);
+            
+            var nextText = null as string;
             var nextIcon = null as Texture2D;
+            
+            if (menuOptionsComponents.Length > 0) {
+                var menuOptions = menuOptionsComponents[0];
+                nextText = menuOptions.nextPageButtonTitle;
+                nextIcon = menuOptions.nextPageButtonIcon;
+
+                if (menuOptionsComponents.Length > 1) {
+                    Debug.LogWarning(
+                        $"Multiple MenuOptions components found. Using first one: {menuOptionsComponents[0].name}");
+                }
+            }
             
             //if (components.Length == 0 || rootMenu == null) return;
 
@@ -64,15 +80,74 @@ namespace org.Tayou.AmityEdits {
             //     
             // }
             
-            GetVRCFFields(avatarDescriptor, out string vrcfNextText, out var vrcfNextIcon );
             
-            nextText ??= vrcfNextText;
+            
+            GetVrcfFields(avatarDescriptor, out string vrcfNextText, out var vrcfNextIcon );
+            
+            nextText ??= vrcfNextText ?? "Next";
             nextIcon ??= vrcfNextIcon;
             
-            // TODO: deduplicate "next page" menus
-            // TODO: make sure max menu size is respected (8 per page) 
+            if (rootMenu != null) {
+                FixMenu(rootMenu, nextText, nextIcon, new HashSet<VRCExpressionsMenu>());
+            }
+        }
+
+        private void FixMenu(VRCExpressionsMenu menu, string nextText, Texture2D nextIcon, HashSet<VRCExpressionsMenu> visited) {
+            if (menu == null || visited.Contains(menu)) return;
+            visited.Add(menu);
             
-            
+            Debug.Log($"[FixMenus] Parsing menu {menu.name}");
+
+            // Important: we must recurse into existing submenus BEFORE we might move them to a next page
+            // to ensure they are also fixed.
+            for (int i = 0; i < menu.controls.Count; i++) {
+                var control = menu.controls[i];
+                if (control != null && control.type == VRCExpressionsMenu.Control.ControlType.SubMenu && control.subMenu != null) {
+                    FixMenu(control.subMenu, nextText, nextIcon, visited);
+                }
+            }
+
+            if (menu.controls.Count > 8) {
+                VRCExpressionsMenu nextPageMenu = null;
+                
+                // Look for existing "Next" or "More" menu
+                for (int i = 0; i < menu.controls.Count; i++) {
+                    var control = menu.controls[i];
+                    if (control != null && control.type == VRCExpressionsMenu.Control.ControlType.SubMenu && 
+                        (nextPageButtonNames.Contains(control.name) || (!string.IsNullOrEmpty(nextText) && control.name == nextText))) {
+                        nextPageMenu = control.subMenu;
+                        
+                        menu.controls.RemoveAt(i);
+                        break;
+                    }
+                }
+
+                if (nextPageMenu == null || nextPageMenu == menu) {
+                    nextPageMenu = ScriptableObject.CreateInstance<VRCExpressionsMenu>();
+                    nextPageMenu.name = menu.name + " (Next)";
+                    if (!string.IsNullOrEmpty(AssetDatabase.GetAssetPath(menu))) {
+                        AssetDatabase.AddObjectToAsset(nextPageMenu, menu);
+                    }
+                }
+
+                // Move excess controls to the next page
+                while (menu.controls.Count > 7) {
+                    nextPageMenu.controls.Add(menu.controls[7]);
+                    menu.controls.RemoveAt(7);
+                }
+
+                // Add the next page control
+                menu.controls.Add(new VRCExpressionsMenu.Control {
+                    name = string.IsNullOrEmpty(nextText) ? "Next" : nextText,
+                    icon = nextIcon,
+                    type = VRCExpressionsMenu.Control.ControlType.SubMenu,
+                    subMenu = nextPageMenu
+                });
+                
+                // Recurse into the next page menu as it might now have > 8 controls
+                visited.Remove(nextPageMenu); 
+                FixMenu(nextPageMenu, nextText, nextIcon, visited);
+            }
         }
 
         private VRCExpressionsMenu GetTargetMenu(MenuLocation menuOperationTargetMenu, VRCExpressionsMenu rootMenu) {
@@ -84,7 +159,10 @@ namespace org.Tayou.AmityEdits {
         }
 
         // Use VRCFuryFeatureUtils to look for OverrideMenuSettings feature on the avatar
-        private void GetVRCFFields(VRCAvatarDescriptor avatarDescriptor, out string vrcfNextText, out Texture2D vrcfNextIcon) {
+        private void GetVrcfFields(VRCAvatarDescriptor avatarDescriptor, out string vrcfNextText, out Texture2D vrcfNextIcon) {
+            vrcfNextText = null;
+            vrcfNextIcon = null;
+            
             var overrideMenuSettings = VRCFuryFeatureUtils.GetOverrideMenuSettingsModel(avatarDescriptor.gameObject, true);
             if (overrideMenuSettings != null) {
                 if (VRCFuryFeatureUtils.TryReadOverrideMenuSettings(overrideMenuSettings, out vrcfNextText, out vrcfNextIcon)) {
@@ -95,9 +173,6 @@ namespace org.Tayou.AmityEdits {
             } else {
                 Debug.Log("[Amity] No VRCFury OverrideMenuSettings feature found on this avatar");
             }
-
-            vrcfNextText = "";
-            vrcfNextIcon = null;
         }
     }
 }
